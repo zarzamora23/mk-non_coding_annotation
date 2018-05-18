cb13744d4cae88a8503d81f8f4f8dceb2868c4d1
Eugenia Zarza <eugenia.zarza@gmail.com>
2018-05-17 23:08:48 -0500

    deleted large files

---
+++ mk-ORegAnno_2_bed/config.mk
@@ -0,0 +1,43 @@
+
+#Genome version depends on species and genome build, as for May 16th 2018, ORegAnno includes information for the following genomes
+#Species Build
+#Arabidopsis thaliana	tair10
+#Bos taurus	bosTau2
+#Bos taurus	bosTau3
+#Bos taurus	bosTau4
+#Branchiostoma floridae	braFlo1
+#Caenorhabditis briggsae	CB25
+#Caenorhabditis briggsae	cb3
+#Caenorhabditis briggsae	cbJul2002
+#Caenorhabditis elegans	ce10
+#Caenorhabditis elegans	ce2
+#Caenorhabditis elegans	ce4
+#Caenorhabditis elegans	ce6
+#Canis lupus familiaris	canFam2
+#Canis lupus familiaris	canFam3
+#Ciona intestinalis	CI
+#Ciona intestinalis	CI2
+#Ciona savignyi	CSAV2
+#Danio rerio	danRer4
+#Danio rerio	danRer5
+#Drosophila melanogaster	dm2
+#Drosophila melanogaster	dm3
+#Drosophila melanogaster	dm6
+#Escherichia coli K-12	eschColi
+#Gallus gallus	galGal3
+#Gallus gallus	galGal4
+#Homo sapiens	hg18
+#Homo sapiens	hg19
+#Homo sapiens	hg38
+#Mus musculus	mm10
+#Mus musculus	mm8
+#Mus musculus	mm9
+#Rattus norvegicus	rn4
+#Rattus norvegicus	rn5
+#Rattus norvegicus	rn6
+#Saccharomyces cerevisiae	sacCer1
+#Takifugu rubripes	fr1
+#Takifugu rubripes	fr2
+#Takifugu rubripes	fr3
+
+Genome_Version=hg19 
